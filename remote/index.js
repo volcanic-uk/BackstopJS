961e36f3d963c92a957063db98026c003489af6e
Garris Shpon <garris@me.com>
2019-05-31 00:41:00 -0700

    add backstop approve

--- remote/index.js
+++ remote/index.js
@@ -9,11 +9,8 @@ var PATH_TO_CONFIG = path.resolve(process.cwd(), 'backstop');
 var _config = require(PATH_TO_CONFIG);
 
 module.exports = function (app) {
-
-
     app._backstop = app._backstop || {};
     app._backstop.testCtr = 0;
-    app._backstop.viewCtr = 0;
     app._backstop.tests = {};
 
     app.use(express.json({limit: '2mb'})); // support json encoded bodies
@@ -45,7 +42,7 @@ module.exports = function (app) {
         scenarioId: req.params.scenarioId, 
         vid: app._backstop.testCtr
       };
-console.log('Starting backstop with:', result);
+
       backstop('test', {config}).then(
         () => {
           result.ok = true;
@@ -59,13 +56,23 @@ console.log('Starting backstop with:', result);
     });
     
     app.get('/dview/:testId/:scenarioId', (req, res) => {
-      app._backstop.viewCtr++;
-      console.log('Dynamic view request for ' + app._backstop.viewCtr, req.params.testId, req.params.scenarioId);
+      console.log('Dynamic view request for ' + req.params.testId, req.params.scenarioId);
       try {
         res.send(app._backstop.tests[req.params.testId][req.params.scenarioId].content);
       } catch (err) {
         console.log(err);
         res.send(`${req.params.testId} ${req.params.scenarioId}` + err);
+      }
+    });
+    
+    app.get('/approve', async (req, res) => {
+      console.log('backstop approve ' + req.query.filter);
+      try {
+        await backstop('approve', {filter: req.query.filter || ''});
+        res.send('OK ' + req.query.filter);
+      } catch (err) {
+        console.log(err);
+        res.send('FAILED ' + err);
       }
     });
 };

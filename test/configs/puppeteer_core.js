64af3700df76551b108f0f633faebe80d7c3f1c8
Garris Shpon <garris@me.com>
2019-06-14 09:18:02 -0700

    use puppeteer-core instead of SKIP_CHROMIUM

---
+++ test/configs/puppeteer_core.js
@@ -0,0 +1,42 @@
+const EXECUTABLE_PATH = `/Applications/Google Chrome.app/Contents/MacOS/Google Chrome`;
+
+module.exports = {
+  id: `backstop_alt`,
+  viewports: [
+    {
+      label: 'phone',
+      width: 320,
+      height: 480
+    },
+    {
+      label: 'tablet',
+      width: 1024,
+      height: 768
+    }
+  ],
+  onBeforeScript: `puppet/onBefore.js`,
+  onReadyScript: `puppet/onReady.js`,
+  scenarios: [
+    {
+      label: 'Simple',
+      url: 'https://garris.github.io/BackstopJS/'
+    }
+  ],
+  paths: {
+    bitmaps_reference: 'backstop_data/bitmaps_reference',
+    bitmaps_test: 'backstop_data/bitmaps_test',
+    engine_scripts: 'backstop_data/engine_scripts',
+    html_report: 'backstop_data/html_report',
+    ci_report: 'backstop_data/ci_report'
+  },
+  report: ['browser'],
+  engine: 'puppet',
+  engineOptions: {
+    executablePath: EXECUTABLE_PATH,
+    args: ['--no-sandbox']
+  },
+  asyncCaptureLimit: 10,
+  asyncCompareLimit: 50,
+  debug: false,
+  debugWindow: false
+};
